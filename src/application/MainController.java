39873c29aef5fcf676e64fe85f571cfabf43ae36
Lee Hwang Tran <lehoangtran289@gmail.com>
2020-05-27 21:42:05 +0700

    add merge sort DOTS

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -170,7 +170,6 @@ public class MainController implements Initializable {
 			
 		case Constants.MERGE:
 			sortTask = new MergeSort(arraySize, delay, curGraphType, mainPane, service);
-			textArea.setText("Not yet implement ^^");
 			break;
 
 		default:
